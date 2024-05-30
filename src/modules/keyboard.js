b2e2da2392e3a3cbfa3135b8ded68c8a410b112e
Syntactic Newman <146788478+syntacticnewman@users.noreply.github.com>
2024-05-30 15:17:15 +0200

    refactor: move layout to keyboard module

--- src/modules/keyboard.js
+++ src/modules/keyboard.js
@@ -1,3 +1,7 @@
+export const layout = {
+  en: "qwertyuiop\nasdfghjkl\nEzxcvbnmB",
+};
+
 export const isLetter = (key) => /^[A-Za-z\u00f1\u00d1]$/.test(key);
 
 export const isBackSpace = (key) => key === "Backspace";
