f294da89c7d57c0b3824f71c9f9757d92e43bca6
Syntactic Newman <146788478+syntacticnewman@users.noreply.github.com>
2024-06-05 01:03:24 +0200

    refactor: remove static functions

--- src/components/virtual-keyboard.js
+++ src/components/virtual-keyboard.js
@@ -4,6 +4,76 @@ const LAYOUT = {
   en: "qwertyuiop\nasdfghjkl\nEzxcvbnmB",
 };
 
+const createVirtualKey = (key) => {
+  const virtualKey = document.createElement("button");
+
+  virtualKey.classList.add("virtual-key");
+
+  switch (key) {
+    case /* Enter */ "E":
+      virtualKey.textContent = "Enter";
+      virtualKey.dataset.key = "Enter";
+      virtualKey.classList.add("special");
+      break;
+
+    case /* Backspace */ "B":
+      virtualKey.textContent = "Del";
+      virtualKey.dataset.key = "Backspace";
+      virtualKey.classList.add("special");
+      break;
+
+    default: /* letter */
+      virtualKey.textContent = key;
+      virtualKey.dataset.key = key;
+  }
+
+  return virtualKey;
+};
+
+const createKeyRow = (keys) => {
+  const keyRow = document.createElement("div");
+
+  keyRow.classList.add("key-row");
+
+  keyRow.append(...keys.split("").map(createVirtualKey));
+
+  return keyRow;
+};
+
+const createKeyboardLayout = (layout) => {
+  const keyboardLayout = document.createElement("div");
+
+  keyboardLayout.classList.add("keyboard-layout");
+
+  keyboardLayout.append(...layout.split("\n").map(createKeyRow));
+
+  return keyboardLayout;
+};
+
+const createVirtualKeyboard = (layout) => {
+  const virtualKeyboard = document.createElement("div");
+
+  virtualKeyboard.classList.add("virtual-keyboard");
+
+  virtualKeyboard.appendChild(createKeyboardLayout(layout));
+
+  virtualKeyboard.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    if ("BUTTON" === event.target.tagName) {
+      document.dispatchEvent(
+        new CustomEvent("virtualkeypressed", {
+          detail: {
+            key: event.target.dataset.key,
+          },
+        })
+      );
+    }
+  });
+
+  return virtualKeyboard;
+};
+
 class VirtualKeyboard extends HTMLElement {
   connectedCallback() {
     const language = document.documentElement.lang;
@@ -18,79 +88,7 @@ class VirtualKeyboard extends HTMLElement {
       layout = LAYOUT[defaultLanguage];
     }
 
-    this.appendChild(VirtualKeyboard.create(layout));
-  }
-
-  static create(layout) {
-    const virtualKeyboard = document.createElement("div");
-
-    virtualKeyboard.classList.add("virtual-keyboard");
-
-    virtualKeyboard.appendChild(VirtualKeyboard.createKeyboardLayout(layout));
-
-    virtualKeyboard.addEventListener("click", (event) => {
-      event.preventDefault();
-
-      if ("BUTTON" === event.target.tagName) {
-        document.dispatchEvent(
-          new CustomEvent("virtualkeypressed", {
-            detail: {
-              key: event.target.dataset.key,
-            },
-          })
-        );
-      }
-    });
-
-    return virtualKeyboard;
-  }
-
-  static createKeyboardLayout(layout) {
-    const keyboardLayout = document.createElement("div");
-
-    keyboardLayout.classList.add("keyboard-layout");
-
-    keyboardLayout.append(
-      ...layout.split("\n").map(VirtualKeyboard.createKeyRow)
-    );
-
-    return keyboardLayout;
-  }
-
-  static createKeyRow(keys) {
-    const keyRow = document.createElement("div");
-
-    keyRow.classList.add("key-row");
-
-    keyRow.append(...keys.split("").map(VirtualKeyboard.createVirtualKey));
-
-    return keyRow;
-  }
-
-  static createVirtualKey(key) {
-    const btn = document.createElement("button");
-
-    btn.classList.add("virtual-key");
-
-    switch (key) {
-      case /* Enter */ "E":
-        btn.textContent = "Enter";
-        btn.dataset.key = "Enter";
-        btn.classList.add("special");
-        break;
-
-      case /* Backspace */ "B":
-        btn.textContent = "Del";
-        btn.dataset.key = "Backspace";
-        btn.classList.add("special");
-        break;
-
-      default: /* letter */
-        btn.textContent = key;
-        btn.dataset.key = key;
-    }
-
-    return btn;
+    this.appendChild(createVirtualKeyboard(layout));
   }
 }
 
