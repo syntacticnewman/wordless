a6df8dc13f548cd146e2e8d52e920d4c3d0757e2
Syntactic Newman <146788478+syntacticnewman@users.noreply.github.com>
2024-06-23 14:48:48 +0200

    chore: add docs to ui controller

--- src/controllers/ui.js
+++ src/controllers/ui.js
@@ -1,14 +1,29 @@
+/**
+ * Selects all the guess rows.
+ */
 const queryAllRows = () => document.querySelectorAll(".guess.row");
 
+/**
+ * Selects a single guess or by index.
+ */
 const queryRow = (index) => queryAllRows().item(index);
 
+/**
+ * Selects all boxes (letters) within a guess row.
+ */
 const queryBoxes = (row) => row.querySelectorAll(".box");
 
+/**
+ * Selects all the virtual key from the virtual keyboard.
+ */
 const queryVirtualKeys = () =>
   document
     .querySelector("virtual-keyboard")
     .shadowRoot.querySelectorAll(".virtual-key");
 
+/**
+ * Renders the letter as user is typing on the current guess row given by the index.
+ */
 const renderLetters = (index, letters) => {
   const row = queryRow(index);
 
@@ -26,6 +41,10 @@ const renderLetters = (index, letters) => {
   });
 };
 
+/**
+ * Renders the feedback for each letter,
+ * whether is correct (green), incorrect (gray) or wrong (yellow) by flipping the card.
+ */
 const renderLettersFeedback = (index, feedback) => {
   const row = queryRow(index);
 
@@ -36,12 +55,18 @@ const renderLettersFeedback = (index, feedback) => {
   });
 };
 
+/**
+ * Renders the invalid guess feedback by highlighting it in red.
+ */
 const renderInvalidGuessFeedback = (index) => {
   const row = queryRow(index);
 
   row.classList.add("guess--invalid");
 };
 
+/**
+ * Renders feedback when user wins by adding a nice choreography animation to the guess.
+ */
 const renderGameOverFeedback = (index, win) => {
   if (!win) return; // TODO: feedback for losing?
 
@@ -57,20 +82,32 @@ const renderGameOverFeedback = (index, win) => {
   }, 500);
 };
 
+/**
+ * Selects the loader spinner.
+ */
 const querySpinner = () => document.querySelector(".spinner");
 
+/**
+ * Animates the loader spinner.
+ */
 const startLoading = () => {
   const spinner = querySpinner();
 
   spinner.classList.add("spinner--loading");
 };
 
+/**
+ * Stops animation on the loader spinner.
+ */
 const stopLoading = () => {
   const spinner = querySpinner();
 
   spinner.classList.remove("spinner--loading");
 };
 
+/**
+ * Renders the feedback on each key of the virtual keyboard.
+ */
 const renderVirtualKeysFeedback = (keyHistory) => {
   // wait for the flip animation to complete
   setTimeout(() => {
