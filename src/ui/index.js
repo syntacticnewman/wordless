29aad96eb356cb7d758ce588a403ff48cac2265c
Isaac Ramírez <146788478+syntacticnewman@users.noreply.github.com>
2024-07-19 14:41:42 +0200

    feat: close about modal on backdrop click (#69)
    * refactor: about modal
    
    * feat: close modal on backdrop click
    
    * fix: about modal styles

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,15 +1,8 @@
-import GA from "../services/analytics.js";
 import { loadCustomComponents } from "./components/index.js";
-import { initAboutModal } from "./modals.js";
+import { initModals } from "./modals/index.js";
 
 const init = () => {
-  // initialize modals
-  initAboutModal({
-    onShow: () => GA.trackAboutModal(true),
-    onClose: () => GA.trackAboutModal(false),
-  });
-
-  // load custom components
+  initModals();
   loadCustomComponents();
 };
 
---
+++ src/ui/modals/about-modal.js
@@ -0,0 +1,45 @@
+import keyboard from "../../modules/keyboard.js";
+import {
+  queryAboutModal,
+  queryCloseAboutModalBtn,
+  queryShowAboutModalBtn,
+} from "../queries.js";
+
+const init = ({ onShow, onClose }) => {
+  const modal = queryAboutModal();
+  const showBtn = queryShowAboutModalBtn();
+  const closeBtn = queryCloseAboutModalBtn();
+
+  // handle Esc key
+  modal.addEventListener("keydown", (event) => {
+    if (keyboard.isEsc(event.key)) {
+      event.stopPropagation();
+      modal.close();
+    }
+  });
+
+  // handle click on backdrop
+  modal.addEventListener("click", (event) => {
+    if (modal === event.target) {
+      modal.close();
+    }
+  });
+
+  // show button
+  showBtn.addEventListener("click", () => {
+    modal.showModal();
+    onShow();
+  });
+
+  // close button
+  closeBtn.addEventListener("click", () => {
+    modal.close();
+  });
+
+  // handle close event
+  modal.addEventListener("close", () => {
+    onClose();
+  });
+};
+
+export default { init };
